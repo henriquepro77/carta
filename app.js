3ed81bf9dd6a724bf7a4aa99708c7ffbed753eab
henriquepro77 <hg24321@gmail.com>
2024-07-10 22:22:29 -0400

    carta

---
+++ app.js
@@ -0,0 +1,18 @@
+alert('Bem vindo ao site Carta Aberta, Se vc for a pessoa clique "Ok"');
+let numeroMaximo = 10;
+let numeroSecreto = 17;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Digite o dia do meu aniversario`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+    }
+}
+
+alert(`isso ai voce sabia mesmo!! espero que goste.`);
